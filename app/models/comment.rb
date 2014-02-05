6177a0d3e250a969cce6504de6ee3a0d90b982ef
Sebastian Hebel <sebah1989@gmail.com>
2014-02-05 12:27:22 +0100

    Comment should be market as abusive when has 3 negative votes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,15 @@ class Comment
   include Mongoid::Timestamps
 
   field :body, type: String
+  field :abusive, type: Boolean, default: false
 
   belongs_to :post
   belongs_to :user
+  has_many :votes, dependent: :destroy
+
+  def check_abusive
+    if votes.where({ value: -1 }).count >= 3
+      update_attributes({ abusive: true })
+    end
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,16 @@
 class Vote
+	include Mongoid::Document
+  include Mongoid::Timestamps
+
+  after_create :check_comment
+
+  field :value, type: Integer
+
+  belongs_to :user
+  belongs_to :comment
+
+  protected
+  def check_comment
+  	comment.check_abusive
+  end
 end
