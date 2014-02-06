4ea3953a78a1d338e45a879f28c2fa7f4958824f
Sebastian Hebel <sebah1989@gmail.com>
2014-02-06 11:12:28 +0100

    permit name parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,10 @@ class ApplicationController < ActionController::Base
   end
 
   before_filter :authenticate_user!
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 end
