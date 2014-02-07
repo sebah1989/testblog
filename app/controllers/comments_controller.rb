1b51c9161146de1d1fcbf00a07425b412cead97f
Sebastian Hebel <sebah1989@gmail.com>
2014-02-07 18:10:28 +0100

    changed voting to voting in ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  respond_to :html, :json, :js
+
 	expose(:post)
   expose(:comment, attributes: :comment_params)
   expose(:comments, ancestor: :post) 
@@ -9,23 +11,23 @@ class CommentsController < ApplicationController
 
   def create
     if comment.save
-      redirect_to post_path(post)
+      respond_with(post)
     end
   end
 
   def vote_up
     vote_comment.vote_up(current_user.id)
-    redirect_to post
+    respond_with(post)
   end
 
   def vote_down
     vote_comment.vote_down(current_user.id)
-    redirect_to post
+    respond_with(post)
   end
 
   def mark_as_not_abusive
     vote_comment.mark_as_not_abusive
-    redirect_to post
+    respond_with(post)
   end
   
   private
